26aa34e08991eff3b2b21a1e88ab11c8f945c334
metaraine <raineorshine@gmail.com>
2014-04-14 17:57:08 -0600

    Test Drive 101: Add more granular assertions to spec.js

--- client-js/test-drive-101/spec.js
+++ client-js/test-drive-101/spec.js
@@ -2,6 +2,8 @@
 
 // You should be able to remove elements from an array.
 assertArraysEqual(splice(['a','b','c','d','e'], 2, 2),  ['a','b','e']);
+assertArraysEqual(splice(['a','b','c','d','e'], 1, 1),  ['a','c','d','e']);
+assertArraysEqual(splice(['x','y','z'], 1, 1),  ['x','z']);
 
 // The original array should remain unchanged (pure function).
 var a = ['a','b','c','d','e'];
@@ -10,6 +12,7 @@ assertArraysEqual(a,  ['a','b','c','d','e']);
 
 // You can insert an item with the fourth argument.
 assertArraysEqual(splice(['a','b','c','d','e'], 1, 2, 'z'),  ['a','z','d','e']);
+assertArraysEqual(splice(['a','b','c','d','e'], 2, 1, 'hi'),  ['a','b','hi','d','e']);
 
 // You can insert elements without removing anything.
 assertArraysEqual(splice(['a','b','c','d','e'], 1, 0, 'z'),  ['a','z','b','c','d','e']);
