10ddb938b894c7ad64149fd5d497b8a482b2bfc6
devaio <robert.camp@outlook.com>
2015-10-12 07:17:54 -0600

    update to angular

--- mongodb/online-job-app/app.js
+++ mongodb/online-job-app/app.js
@@ -1,19 +1,24 @@
+// Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
 
+// Create Express App Object \\
 var app = express();
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+
+// Routes \\
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.sendFile('html/index.html', {root : './public'});
 });
 
 // displays a list of applicants
 app.get('/applicants', function(req, res){
-	res.render('applicants')
+	res.sendFile('html/applicants.html', {root : './public'});
 });
 
 // creates and applicant
@@ -23,6 +28,11 @@ app.post('/applicant', function(req, res){
 	res.send('No funciona');
 });
 
-var server = app.listen(8441, function() {
-	console.log('Express server listening on port ' + server.address().port);
-});
+
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
--- mongodb/online-job-app/public/javascripts/main.js
+++ mongodb/online-job-app/public/javascripts/main.js
@@ -1,3 +1,11 @@
-$(function(){
+angular.module('JobApp', [])
 
-});
+angular.module('JobApp')
+	.controller('homeController', ['$scope', function($scope){
+		
+	}]);
+
+angular.module('JobApp')
+	.controller('applicantController', ['$scope', function($scope){
+		
+	}]);
