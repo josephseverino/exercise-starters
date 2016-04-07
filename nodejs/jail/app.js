3303860d2662757bda1140f1981579da0e6ea4a5
rserota <raphael.serota@gmail.com>
2016-04-07 11:31:34 -0600

    removed solution code from jail exercise

--- nodejs/jail/app.js
+++ nodejs/jail/app.js
@@ -51,37 +51,13 @@ passport.deserializeUser(function(id, done) {
 // When someone tries to log in to our site, how do we determine that they are who they say they are?
 var bcrypt = require('bcryptjs')
 passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-                return done(null, false);
-            }
-            return done(null,user)
-            // If we got this far, then we know that the user exists. But did they put in the right password?
-            bcrypt.compare(password, user.password, function(error, response){
-                if (response === true){
-                    return done(null,user)
-                }
-                else {
-                    return done(null, false)
-                }
-            })
-        });
-    }
+    // write local strategy here
 ));
 
 app.isAuthenticated = function(req, res, next){
-    // If the current user is logged in...
-    if(req.isAuthenticated()){
-    // Middleware allows the execution chain to continue.
-        return next();
-    }
-    else {
-        // If not, redirect to login
-        console.log('get outta here!')
-        res.redirect('/');
-    }
+    // If the current user is logged in, allow them through
+    // else, kick them out to the login page.
+
 }
 
 app.post('/signup', function(req, res){
@@ -107,24 +83,16 @@ app.post('/signup', function(req, res){
 })
 
 app.post('/login', function(req, res, next){
-    passport.authenticate('local', function(err, user, info) {
-        console.log(err,user,info)
-        if (err) { return next(err); }
-        if (!user) { return res.send({error : 'something went wrong :('}); }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.send({success:'success'});
-        });
-    })(req, res, next);
+    // use your local strategy here.
 })
 
 app.get('/', function(req, res){
     res.sendFile('/html/login.html', {root: './public'})
 })
 app.get('/api/me', function(req,res){
-    res.send(req.user) //
+    // send down the logged-in user.
 })
-app.get('/jail', app.isAuthenticated, function(req, res){
+app.get('/jail', function(req, res){
     res.sendFile('/html/jail.html', {root: './public'})
 })
 app.get('/lobby', function(req, res){
